55f6269919701dcaf5a2e788c13b95538e82453f
rakeshrengaraj <rakeshrengaraj@bitbucket.org>
2022-12-27 17:01:02 +0530

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+
+const notes = require('./data/notes')
+
+const dotenv = require('dotenv');
+dotenv.config()
+
+
+app.get('/',(req, res) => {
+    res.send("Welcome to notes app")
+})
+
+app.get('/api/notes', (req,res) => {
+    res.json(notes)
+})
+
+app.get('/api/notes/:id', (req,res) => {
+    const note = notes.find((n) => {
+        n._id == req.params.id
+    })
+    console.log(note)
+    res.json(note)
+})
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+    console.log(`Server is up and running on port ${PORT}`);
+})
